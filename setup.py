20dbc2e5c72d7845966e3f61456c5b1d91a37e05
OM M PATEL <96344622+PATELOM925@users.noreply.github.com>
2024-07-03 10:59:55 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import find_packages, setup
 
 setup(
     name = "Food Chatbot",
-    version= "0.0.0",
+    version= "0.0.0", 
     author= "OM M Patel",
-    author_email= "IAMPATELOM@GMAIL.COM", 
+    author_email= "iampatelom@gmail.com", 
     packages= find_packages(),
     install_requires=[]
 )
